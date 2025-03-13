5d51cf80427a4f2f0f52ce5f490e4932c6e8628e
sw-lee-dev <lsw6212@naver.com>
2025-03-13 11:51:37 +0900

    2025.03.13 11:50 first

---
+++ src/test/java/com/lsw/memories_back/MemoriesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsw.memories_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoriesBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
