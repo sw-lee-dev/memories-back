f2eb74113fc179b66b3918b433a4c23a1f657801
sw-lee-dev <lsw6212@naver.com>
2025-03-13 17:29:53 +0900

    2025.03.13 17:50

--- src/main/java/com/lsw/memories_back/repository/UserRepository.java
+++ src/main/java/com/lsw/memories_back/repository/UserRepository.java
@@ -8,4 +8,6 @@ import com.lsw.memories_back.common.entity.UserEntity;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, String> {
   
+  boolean existsByUserId(String userId);
+
 }
