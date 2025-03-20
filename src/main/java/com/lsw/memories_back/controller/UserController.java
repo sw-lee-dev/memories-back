5df546a5443a22a8260b3b9ccf2fa37282f36e69
sw-lee-dev <lsw6212@naver.com>
2025-03-20 14:51:38 +0900

    2025.03.20 14:50

---
+++ src/main/java/com/lsw/memories_back/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.lsw.memories_back.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lsw.memories_back.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserController {
+  
+  private final UserService userService;
+
+}
