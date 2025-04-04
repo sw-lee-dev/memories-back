e2ef4182de23518d3fe56a2fd24f4fefc4fd2886
sw-lee-dev <lsw6212@naver.com>
2025-04-04 17:23:14 +0900

    2025.04.04 board delete

--- src/main/java/com/lsw/memories_back/service/BoardService.java
+++
@@ -1,9 +0,0 @@
-package com.lsw.memories_back.service;
-
-import org.springframework.http.ResponseEntity;
-
-import com.lsw.memories_back.common.dto.response.GetBoardResponseDto;
-
-public interface BoardService {
-  ResponseEntity<? super GetBoardResponseDto> getBoard(int receptionNumber);
-}
